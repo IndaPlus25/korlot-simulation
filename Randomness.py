8d3df1f125b0b4cb43f3d2f3db88ccda85399bba
FilipK <korlot@kth.se>
2025-11-21 08:09:42 +0100

    added

---
+++ Randomness.py
@@ -0,0 +1,21 @@
+from graphics import *
+import random as random
+
+x_val = random.randint(0, 1600)
+y_val = random.randint(0, 800)
+
+# Create Window and object inside it
+window = GraphWin("Randomness", 1600, 800, autoflush=True)
+target = Circle(Point(x , y), 30)
+target.setFill("red")
+target.draw(window)
+
+#move the object at random
+def movement():
+        #change position
+        choice_x = random.randint(-1,1)
+        x_val = x_val + choice_x
+
+        choice_y = random.randint(-1,1)
+        y_val = y_val + choice_y
+
