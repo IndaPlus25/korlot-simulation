7d6bbef0b4e90560e6026dd9814e13a6ecf14264
FilipK <korlot@kth.se>
2025-11-21 08:21:41 +0100

    added lines

--- Randomness.py
+++ Randomness.py
@@ -4,7 +4,8 @@ import random as random
 x_val = random.randint(0, 1600)
 y_val = random.randint(0, 800)
 
-# Create Window and object inside it
+# Create Window and object inside it 
+# Note not my created code
 window = GraphWin("Randomness", 1600, 800, autoflush=True)
 target = Circle(Point(x , y), 30)
 target.setFill("red")
@@ -19,3 +20,8 @@ def movement():
         choice_y = random.randint(-1,1)
         y_val = y_val + choice_y
 
+# Add noise to the randomness
+
+
+while true:
+    #Impliment updating movement
